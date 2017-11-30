9bcb27f0a224a4034d596e5380fdd5450a978dd6
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-30 21:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
 class Driver {
-  constructor(name, startDate){
+  constructor (name, startDate){
     this.name = name;
     this.startDate = new Date(startDate);
   }
@@ -12,3 +12,9 @@ class Driver {
   }
 }
 
+class Route {
+  constructor (beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
