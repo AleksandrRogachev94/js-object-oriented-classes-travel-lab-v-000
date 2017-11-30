0c2032fcffca2db3378b4ddee26d998e8908fa7c
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-30 21:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ class Driver {
   }
   yearsExperienceFromBeginningOf(year){
     let endDate = new Date(year, 0, 1)
-    let totalYears = (endDate - this.startDate)/365*24*60*60*60
-    return totalYears;
+    let totalYears = (endDate - this.startDate)/365*24*60*60*1000
+    return parseInt(totalYears);
   }
 }
 
